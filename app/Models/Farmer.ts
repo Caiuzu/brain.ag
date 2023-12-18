c495d4e049b0830fd5fa3d2ba8b4a70077f49f5f
Caio Souza <caio.7s@outlook.com>
2023-12-17 23:10:36 -0300

    feat: criando modelo e migracao para Farmer

---
+++ app/Models/Farmer.ts
@@ -0,0 +1,19 @@
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm';
+import Farm from './Farm';
+
+export default class Farmer extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number;
+
+  @column()
+  public name: string;
+
+  @column()
+  public document: string;
+
+  @column()
+  public farmId: number;
+
+  @belongsTo(() => Farm)
+  public farm: BelongsTo<typeof Farm>;
+}

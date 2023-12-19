21506f37a3c91aa5802d4dc8c0533e7f52719a75
Caio Souza <caio.7s@outlook.com>
2023-12-19 06:55:14 -0300

    test: criando testes funcionais para FarmerController

---
+++ tests/functional/farmer/farmer_controller.spec.ts
@@ -0,0 +1,37 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import { test } from '@japa/runner'
+
+test.group('Farmer Controller', (group) => {
+
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction();
+    return () => Database.rollbackGlobalTransaction();
+  });
+
+  test('should list all farmers', async ({ client, assert }) => {
+    const response = await client.get('/api/farmer')
+
+    response.assertStatus(200)
+
+    assert.isArray(response.body())
+    assert.lengthOf(response.body(), 10)
+    assert.equal(response.body()[0].name, 'João Silva')
+    assert.equal(response.body()[9].name, 'Tiago Oliveira')
+  })
+
+  test('should show a farmer', async ({ client, assert }) => {
+    const response = await client.get('/api/farmer/1')
+
+    response.assertStatus(200)
+    assert.equal(response.body().name, 'João Silva')
+  })
+
+  test('should delete a farmer', async ({ client, assert }) => {
+    const response = await client.delete('/api/farmer/1')
+
+    response.assertStatus(200)
+    assert.equal(response.body().message, 'Farmer successfully deleted.')
+  })
+
+})
+
